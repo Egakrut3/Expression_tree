b1e6d55d08012456aee6f1b272858cff1379646b
Egakrut3 <157879824+Egakrut3@users.noreply.github.com>
2025-11-18 11:52:04 +0300

    Initial commit

---
+++ include/Option_manager.h
@@ -0,0 +1,20 @@
+#ifndef OPTION_MANAGER_H
+#define OPTION_MANAGER_H
+
+#include "Common.h"
+
+#define UNKNOWN_OPTION              500
+#define NOT_ENOUGH_OPTION_ARGUMENTS 501
+
+struct Config
+{
+    bool is_help;
+
+    bool is_valid;
+};
+
+errno_t Config_Ctor(Config *config_ptr, size_t argc, char const *const *argv);
+
+errno_t Config_Dtor(Config *config_ptr);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Common.h"
+#include "Option_manager.h"
+
+#undef FINAL_CODE
+#define FINAL_CODE
+
+int main(int const argc, char const *const *const argv) {
+    
+
+    colored_printf(GREEN, BLACK, "\n\n\nCOMMIT GITHUB\n\n");
+    CLEAR_RESOURCES();
+    return 0;
+}
